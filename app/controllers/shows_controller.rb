3fca96e17184569cecdfb6c9076787252a195ac4
ParthDevani <parthdevani2121@gmail.com>
2022-03-07 18:20:38 +0530

    add search for channel,add gem acts_as_favoritor

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,44 +1,35 @@
 class ShowsController < ApplicationController
   before_action :login_user_id
   def index
-      @shows = Show.all.paginate(:page => params[:page], :per_page => 3).order("created_at DESC")
+      @shows = Show.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 3)
       @favourites = Favourite.where(user_id:  @user.id)
   end
 
   def search 
       @search = params[:string]
-      @shows = Show.where("lower(name) LIKE ? OR lower(description) LIKE ?", "%#{@search.downcase}%", "%#{@search.downcase}%").paginate(:page => params[:page], :per_page => 3)
+      @shows = Show.joins(:channel).order('created_at DESC').select("shows.*").where("lower(shows.name) LIKE ? OR lower(channels.name) LIKE ?", "%#{@search.downcase}%", "%#{@search.downcase}%").paginate(:page => params[:page], :per_page => 3)
       @favourites = Favourite.where(user_id:  @user.id)
       render "index"
   end
 
   def favourites
-    @favourites = Favourite.where(user_id:  @user.id).order("created_at DESC")
+    @favourites = @user.favorited_by_type 'Show'
   end
 
   def add_favourite
-    # @show = Show.find(params[:id])
-    # if @show.favorited_by?(@user)
-    #   @user.favorite(@show)
-    #   redirect_to root_path,notice:"#{@show.name} is added to favourites"
-    # else
-    #   redirect_to root_path,notice:"#{@show.name} is already in favourites"
-    # end
-
-    @oldFavourite = Favourite.where(show_id: params[:id],user_id:  @user.id).first
-    if @oldFavourite 
-      redirect_to root_path,notice:"#{@oldFavourite.show.name} is already in favourites"
+    @show = Show.find(params[:id])
+    if !@user.favorited?(@show)
+      @user.favorite(@show)
+      redirect_to root_path,notice:"#{@show.name} is added to favourites"
     else
-      @favourite = Favourite.new(show_id: params[:id],user_id:  @user.id)
-      @favourite.save
-      redirect_to root_path,notice:"#{@favourite.show.name} is added to favourites"
+      redirect_to root_path,notice:"#{@show.name} is already in favourites"
     end
   end
 
   def remove_favourites
-     @favourite = Favourite.find(params[:id])
-     @favourite.destroy
-     redirect_back fallback_location: root_path,notice:"#{@favourite.show.name} is removed from favourites"
+     @show = Show.find(params[:id])
+     @user.unfavorite(@show)
+     redirect_back fallback_location: root_path,notice:"#{@show.name} is removed from favourites"
   end
 
 
